7b925e2e100ab683655af8ea0e654cee3e8936b9
only1stumpy <rei228322@gmail.com>
2026-05-28 03:32:05 +0300

    fix: fix images load 2

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,19 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactCompiler: true,
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'upload.wikimedia.org',
+      },
+      {
+        protocol: 'https',
+        hostname: 'e.cdnmoldcell.md',
+      },
+      {
+        protocol: 'https',
+        hostname: 'encrypted-tbn0.gstatic.com',
       },
     ],
   },
